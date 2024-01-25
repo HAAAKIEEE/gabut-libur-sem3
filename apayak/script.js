58ec796719e7ee83e4edbedca2a83a1ca563f1db
baihaqi <haqi20082004@gmail.com>
2024-01-25 16:42:12 +0700

    input-nama

--- apayak/script.js
+++
@@ -1,9 +0,0 @@
-var hasil = document.getElementById('hasil');
-var submit = document.getElementById('submit');
-submit.onclick = ambilNilai;
-console.log(submit)
-
-function ambilNilai(){
-    var text = document.getElementById('input').vlaue;
-    hasil.innerHTML = text;
-}
